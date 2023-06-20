8d982b9e1d4a7d2465465d99a2dc677b3059a6fa
sho <sho.webfe@gmail.com>
2023-06-21 05:24:46 +0900

    corsの設定

--- apps/graphql/src/main.ts
+++ apps/graphql/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.CLIENT_WEB_URL],
+  });
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
