3220264b62b493158c5d051ced389647c390176c
sho <sho.webfe@gmail.com>
2023-07-06 23:34:24 +0900

    repositoryを受け取るように変更

--- apps/worker/src/application/usecase/regiseter_user_usecase.go
+++ apps/worker/src/application/usecase/regiseter_user_usecase.go
@@ -4,12 +4,17 @@ import (
 	"github.com/sho-ts/folio-driven/src/application/input"
 	"github.com/sho-ts/folio-driven/src/application/output"
 	"github.com/sho-ts/folio-driven/src/domain/entity"
+	"github.com/sho-ts/folio-driven/src/infrastructure/repository"
 )
 
-type RegisterUserUseCase struct{}
+type RegisterUserUseCase struct {
+	creatorRepository repository.ICreatorRepository
+}
 
-func NewRegisterUseCase() *RegisterUserUseCase {
-	return &RegisterUserUseCase{}
+func NewRegisterUseCase(creatorRepository repository.ICreatorRepository) *RegisterUserUseCase {
+	return &RegisterUserUseCase{
+		creatorRepository: creatorRepository,
+	}
 }
 
 func (u *RegisterUserUseCase) Handle(input *input.RegisterUserInput) (*output.RegisteruserOutput, error) {
