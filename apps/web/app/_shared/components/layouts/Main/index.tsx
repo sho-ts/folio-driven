f8972330253340a15ccbd68471aded15d6ef7bcb
sho <sho.webfe@gmail.com>
2023-07-11 01:13:21 +0900

    Main追加

---
+++ apps/web/app/_shared/components/layouts/Main/index.tsx
@@ -0,0 +1,7 @@
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Main = ({ children }: Props) => {
+  return <div className="max-w-7xl mx-auto w-full py-24 px-4">{children}</div>;
+};
