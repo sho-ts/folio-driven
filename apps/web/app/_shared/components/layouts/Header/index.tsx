f4f26d1d5132faacdc365dcaba01859e70eb059f
sho <sho.webfe@gmail.com>
2023-07-11 01:05:50 +0900

    Header作成

---
+++ apps/web/app/_shared/components/layouts/Header/index.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header>
+      <nav className="pl-4 h-14 flex items-center border-b border-neutral-200 border-solid">
+        <div className="max-w-7xl mx-auto w-full">
+          <div className="text-sky-500 text-lg font-bold">FOLIO DRIVEN</div>
+        </div>
+      </nav>
+    </header>
+  );
+};
