a8408e98ee6184277500959ed49d5dcaad388903
sho <sho.webfe@gmail.com>
2023-07-11 03:05:40 +0900

    typo修正

--- apps/web/app/_shared/components/parts/Button/index.tsx
+++ apps/web/app/_shared/components/parts/Button/index.tsx
@@ -8,11 +8,11 @@ type Props<T extends ElementType | FC> = {
   fill?: boolean;
 } & ComponentPropsWithRef<T>;
 
-export const Button = <T extends ElementType | FC = 'button'>({ as: tag, fill, children, clasName, ...props }: Props<T>) => {
+export const Button = <T extends ElementType | FC = 'button'>({ as: tag, fill, children, className, ...props }: Props<T>) => {
   const Tag = tag || 'button';
 
   return (
-    <Tag {...props} className={clsx('rounded py-2 px-6 bg-gradient-to-r from-sky-300 to-sky-400 text-white inline-flex items-center justify-center transition duration-300 ease-in-out hover:opacity-80', fill && 'flex w-full ', clasName)}>
+    <Tag {...props} className={clsx('rounded py-2 px-6 bg-gradient-to-r from-sky-300 to-sky-400 text-white inline-flex items-center justify-center transition duration-300 ease-in-out hover:opacity-80', fill && 'flex w-full ', className)}>
       {children}
     </Tag>
   );
