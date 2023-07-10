9a5f29f72eeb8a9b16c6824c48db1eb3f9013ff0
sho <sho.webfe@gmail.com>
2023-07-11 01:13:13 +0900

    position-fixed設定

--- apps/web/app/_shared/components/layouts/Header/index.tsx
+++ apps/web/app/_shared/components/layouts/Header/index.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header>
-      <nav className="pl-4 h-14 flex items-center border-b border-neutral-200 border-solid">
+      <nav className="z-[10000] fixed t-0 l-0 w-full bg-white pl-4 h-14 flex items-center border-b border-neutral-200 border-solid">
         <div className="max-w-7xl mx-auto w-full">
           <div className="text-sky-500 text-lg font-bold">FOLIO DRIVEN</div>
         </div>
