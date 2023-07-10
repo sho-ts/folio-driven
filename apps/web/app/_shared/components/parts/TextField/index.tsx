9a3fde7fb3ba107052bf0614fc5d863362041e8d
sho <sho.webfe@gmail.com>
2023-07-11 01:50:16 +0900

    labelを使用する

--- apps/web/app/_shared/components/parts/TextField/index.tsx
+++ apps/web/app/_shared/components/parts/TextField/index.tsx
@@ -32,7 +32,7 @@ export const TextField = forwardRef<any, BaseProps>(function TextField({ classNa
         className={clsx('resize-none border-b border-neutral-200 border-solid block px-4 pt-5 pb-1 outline-none focus:border-sky-500 transition duration-300 ease-in-out w-full block', error && 'bg-red-50 focus:border-red-500 border-red-200', styles.body)}
         {...props}
       />
-      <span className="pointer-events-none text-slate-400 flex absolute top-5 left-4 transition duration-300 ease-in-out origin-top-left">{placeholder}</span>
+      <label className="pointer-events-none text-slate-400 flex absolute top-5 left-4 transition duration-300 ease-in-out origin-top-left">{placeholder}</label>
     </div>
   );
 }) as <T extends number | undefined = undefined>(props: Props<T>, ref: T extends number ? HTMLInputElement : HTMLTextAreaElement) => JSX.Element;
