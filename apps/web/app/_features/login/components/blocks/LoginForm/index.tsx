ac03ac645994866d0202d9e51a7c1b07c7549f8b
sho <sho.webfe@gmail.com>
2023-07-12 00:04:43 +0900

    フォーム作成中

--- apps/web/app/_features/login/components/blocks/LoginForm/index.tsx
+++ apps/web/app/_features/login/components/blocks/LoginForm/index.tsx
@@ -2,13 +2,39 @@
 
 import { FormControl } from '@/app/_shared/components/blocks';
 import { Button, TextField } from '@/app/_shared/components/parts';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+
+const schema = z.object({
+  email: z.string().nonempty('メールアドレスは必須です').email('メールアドレスの形式で入力してください'),
+  password: z.string().nonempty('パスワードは必須です'),
+});
+
+type Schema = z.infer<typeof schema>;
 
 export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Schema>({ resolver: zodResolver(schema) });
+
+  const onSubmit = useCallback(
+    (data: Schema) => {
+
+    },
+    []
+  );
+
   return (
     <>
-      <FormControl render={(props) => <TextField fill placeholder="メールアドレス" {...props} />} />
-      <FormControl render={(props) => <TextField fill placeholder="パスワード" {...props} />} />
-      <Button fill>ログイン</Button>
+      <FormControl errors={[errors.email?.message]} render={(props) => <TextField {...register('email')} fill placeholder="メールアドレス" {...props} />} />
+      <FormControl errors={[errors.password?.message]} render={(props) => <TextField {...register('password')} fill placeholder="パスワード" {...props} />} />
+      <Button fill onClick={handleSubmit(onSubmit)}>
+        ログイン
+      </Button>
     </>
   );
 };
