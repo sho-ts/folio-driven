7b9e79cc8c06f1ce38618efae9e82bbb2c139b91
sho <sho.webfe@gmail.com>
2023-07-12 00:04:38 +0900

    スタイル調整

--- apps/web/app/_features/login/components/blocks/FormContainer/index.tsx
+++ apps/web/app/_features/login/components/blocks/FormContainer/index.tsx
@@ -7,7 +7,7 @@ export const FormContainer = ({ children, renderHeading }: Props) => {
   return (
     <div className="mx-auto w-full max-w-2xl">
       {renderHeading?.()}
-      <div className="mx-auto w-full border border-neutral-200 rounded-md py-10 px-4 flex flex-col items-center gap-10 md:px-16">{children}</div>
+      <div className="mx-auto w-full flex flex-col items-center gap-10">{children}</div>
     </div>
   );
 };
