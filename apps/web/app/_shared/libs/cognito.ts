7342062a0c6097972972fbee40761f3d96ff74ce
sho <sho.webfe@gmail.com>
2023-07-13 02:04:45 +0900

    トークンの保存と取得処理を追加

--- apps/web/app/_shared/libs/cognito.ts
+++ apps/web/app/_shared/libs/cognito.ts
@@ -5,7 +5,11 @@ import {
   AuthenticationDetails,
   CognitoUserSession,
   CognitoUserAttribute,
+  CognitoIdToken,
+  CognitoAccessToken,
+  CognitoRefreshToken,
 } from 'amazon-cognito-identity-js';
+import { cookies } from 'next/headers';
 
 const userPool = new CognitoUserPool({
   UserPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID as string,
@@ -58,7 +62,76 @@ export const signIn = (email: string, password: string) => {
   });
 };
 
-/** サインアップ時にCongitoから送信されたコードを認証する */
+export const setTokens = ({
+  idToken,
+  accessToken,
+  refreshToken,
+}: {
+  idToken: CognitoIdToken;
+  accessToken: CognitoAccessToken;
+  refreshToken: CognitoRefreshToken;
+}) => {
+  cookies().set('idToken', idToken.getJwtToken(), {
+    httpOnly: true,
+    secure: true,
+    expires: new Date((idToken.getExpiration() - 10 * 60) * 1000).getTime(),
+  });
+  cookies().set('accessToken', accessToken.getJwtToken(), {
+    httpOnly: true,
+    secure: true,
+    expires: new Date((accessToken.getExpiration() - 10 * 60) * 1000).getTime(),
+  });
+  cookies().set('refreshToken', refreshToken.getToken(), {
+    httpOnly: true,
+    secure: true,
+  });
+};
+
+export const getTokens = async () => {
+  const idToken = cookies().get('idToken')?.value;
+  const accessToken = cookies().get('accessToken')?.value;
+  const refreshToken = cookies().get('refreshToken')?.value;
+
+  if (!refreshToken) throw new Error('ログインしていません');
+
+  if (!idToken || !accessToken) {
+    return await refreshSession(refreshToken);
+  }
+
+  return { idToken, accessToken, refreshToken };
+};
+
+export const refreshSession = (refreshToken: string) => {
+  return new Promise<{
+    idToken: string;
+    accessToken: string;
+    refreshToken: string;
+  }>((resolve, reject) => {
+    const user = userPool.getCurrentUser();
+
+    if (!user) return reject(new Error('不正なユーザー'));
+
+    user.refreshSession(
+      new CognitoRefreshToken({ RefreshToken: refreshToken }),
+      (result: CognitoUserSession, err: Error) => {
+        if (err) return reject(err.message);
+
+        const idToken = result.getIdToken();
+        const accessToken = result.getAccessToken();
+        const refreshToken = result.getRefreshToken();
+
+        setTokens({ idToken, accessToken, refreshToken });
+        resolve({
+          idToken: idToken.getJwtToken(),
+          accessToken: accessToken.getJwtToken(),
+          refreshToken: refreshToken.getToken(),
+        });
+      },
+    );
+  });
+};
+
+/** サインアップ時にCognitoから送信されたコードを認証する */
 export const confirmRegistration = (email: string, code: string) => {
   const user = new CognitoUser({
     Username: email,
