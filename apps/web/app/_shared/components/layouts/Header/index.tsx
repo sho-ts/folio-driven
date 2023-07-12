1b27410c5950fd9eb72f077d0047fe05ce8243ce
sho <sho.webfe@gmail.com>
2023-07-13 02:14:31 +0900

    z-indexを変更

--- apps/web/app/_shared/components/layouts/Header/index.tsx
+++ apps/web/app/_shared/components/layouts/Header/index.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header>
-      <nav className='t-0 l-0 fixed z-[10000] flex h-14 w-full items-center border-b border-solid border-neutral-200 bg-white pl-4'>
+      <nav className='t-0 l-0 fixed z-[5000] flex h-14 w-full items-center border-b border-solid border-neutral-200 bg-white pl-4'>
         <div className='mx-auto w-full max-w-7xl'>
           <div className='text-lg font-bold text-sky-500'>FOLIO DRIVEN</div>
         </div>
