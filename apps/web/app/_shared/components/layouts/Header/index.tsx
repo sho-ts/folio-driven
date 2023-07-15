6ea6a3745fd183d1f7e523525dfe029e6606e4e1
sho <sho.webfe@gmail.com>
2023-07-16 03:03:03 +0900

    非ログイン時にログインボタンを表示

--- apps/web/app/_shared/components/layouts/Header/index.tsx
+++ apps/web/app/_shared/components/layouts/Header/index.tsx
@@ -1,9 +1,14 @@
+import { UserInfo } from './UserInfo';
+
 export const Header = () => {
   return (
     <header>
-      <nav className='t-0 l-0 fixed z-[5000] flex h-14 w-full items-center border-b border-solid border-neutral-200 bg-white pl-4'>
-        <div className='mx-auto w-full max-w-7xl'>
+      <nav className='t-0 l-0 fixed z-[5000] flex h-14 w-full items-center border-b border-solid border-neutral-200 bg-white px-4'>
+        <div className='mx-auto flex w-full max-w-7xl items-center'>
           <div className='text-lg font-bold text-sky-500'>FOLIO DRIVEN</div>
+          <div className='ml-auto'>
+            <UserInfo />
+          </div>
         </div>
       </nav>
     </header>
