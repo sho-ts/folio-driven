a3222c31f0bb3c60f48bb5337015b2ac42578478
sho <sho.webfe@gmail.com>
2023-07-16 23:33:04 +0900

    productImageRepository追加

---
+++ apps/graphql/src/infrastructure/repository/product-image.repository.ts
@@ -0,0 +1,31 @@
+import { ProductImages } from '@/domain/entity/aggregation/product-images.entity';
+import { ProductImage } from '@/domain/entity/product/product-image.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductImageRepository {
+  constructor(
+    @InjectRepository(ProductImage)
+    private readonly repository: Repository<ProductImage>,
+  ) {}
+
+  async search(productImages: ProductImages) {
+    const query = this.repository.createQueryBuilder('productImage').select('productImage');
+
+    if (productImages.productIds) {
+      query.where('productImage.productId in (:...productIds)', {
+        productIds: productImages.productIds,
+      });
+    }
+
+    const [total, items] = await Promise.all([query.getCount(), query.getMany()]);
+
+    const result = new ProductImages();
+    result.total = total;
+    result.items = items;
+
+    return result;
+  }
+}
