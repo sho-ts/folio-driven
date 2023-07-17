3135dd16c4554f49154c64e589ecb9ccef6e6a52
sho <sho.webfe@gmail.com>
2023-07-18 01:43:26 +0900

    投稿詳細仮

---
+++ apps/web/app/(pages)/products/[productId]/page.tsx
@@ -0,0 +1,15 @@
+import { Header, Main } from '@/app/_shared/components/layouts';
+import { Fragment } from 'react';
+
+const ProductDetail = () => {
+  return (
+    <Fragment>
+      <Header title='投稿詳細' />
+      <Main>
+        <div>ProductDetail</div>
+      </Main>
+    </Fragment>
+  );
+};
+
+export default ProductDetail;
